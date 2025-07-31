62a4c3345fc101e6c119cff93252e04951e79bfe
mecanicoraiz <thiago.sejanoski@escola.pr.gov.br>
2025-07-31 11:04:39 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,27 +1,27 @@
 const perguntas = [
     {
-    categoria: "[matematica]",
-    pergunta: "[Quanto é 7x8?]",
-    resposta: "[56]"
+    categoria: "matematica",
+    pergunta: "Quanto é 7x8?",
+    resposta: "56"
     },
     {
-    categoria: "[História]",
-    pergunta: "[ Quem foi o primeiro presidente do Brasil?]",
-    resposta: "[ Deodoro da Fonseca]"
+    categoria: História",
+    pergunta: " Quem foi o primeiro presidente do Brasil",
+    resposta: " Deodoro da Fonseca"
     },
     {
-    categoria: "[Ciência]",
-    pergunta: "[Qual é o gás mais abundante na atmosfera terrestre?]",
-    resposta: "[Nitrogênio (cerca de 78%)]"
+    categoria: "Ciência",
+    pergunta: "Qual é o gás mais abundante na atmosfera terrestre?",
+    resposta: "Nitrogênio (cerca de 78%)"
     },
     {
-    categoria: "[Geografia]",
-    pergunta: "[Qual é o maior oceano do mundo?]",
-    resposta: "[Oceano Pacífico]"
+    categoria: "Geografia",
+    pergunta: "Qual é o maior oceano do mundo?",
+    resposta: "Oceano Pacífico"
     },
     {
-    categoria: "[Português]",
-    pergunta: "[Quantas sílabas tem a palavra cadeira?]",
-    resposta: "[três (ca-dei-ra).]"
+    categoria: "Português",
+    pergunta: "Quantas sílabas tem a palavra cadeira?",
+    resposta: "três (ca-dei-ra)."
     }
     ];
